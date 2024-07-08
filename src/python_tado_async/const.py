f1b328c9ed2681ec59409ac61535c6ef5498409a
Erwin Douna <e.douna@gmail.com>
2024-07-08 18:49:57 +0000

    Adding const

--- src/python_tado_async/const.py
+++ src/python_tado_async/const.py
@@ -2,6 +2,14 @@
 
 from enum import Enum
 
+CONST_MODE_OFF = "OFF"
+CONST_MODE_SMART_SCHEDULE = "SMART_SCHEDULE"
+CONST_MODE_AUTO = "AUTO"
+CONST_MODE_COOL = "COOL"
+CONST_MODE_HEAT = "HEAT"
+CONST_MODE_DRY = "DRY"
+CONST_MODE_FAN = "FAN"
+
 
 class HttpMethod(Enum):
     """HTTP methods."""
