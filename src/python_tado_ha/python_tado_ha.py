4894e20b269b433c8eec5786a13c43176dd182cb
Erwin Douna <e.douna@gmail.com>
2024-07-08 09:49:25 +0000

    Add session

--- src/python_tado_ha/python_tado_ha.py
+++ src/python_tado_ha/python_tado_ha.py
@@ -55,7 +55,7 @@ class Tado:  # pylint: disable=too-many-instance-attributes
         username: str,
         password: str,
         debug: bool | None = None,
-        session: ClientSession | None = None,  # pylint: disable=unused-argument
+        session: ClientSession | None = None,
     ) -> None:
         """Initialize the Tado object.
 
@@ -67,10 +67,13 @@ class Tado:  # pylint: disable=too-many-instance-attributes
         self._username: str = username
         self._password: str = password
         self._debug: bool = debug or False
+        self._session = session
+
         self._headers: dict[str, str] = {
             "Content-Type": "application/json",
             "Referer": "https://app.tado.com/",
         }
+
         self._access_token: str | None = None
         self._token_expiry: float | None = None
         self._refesh_token: str | None = None
