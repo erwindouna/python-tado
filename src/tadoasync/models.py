7472676f67363854cebc6081087424f17f875aa4
Ethem Cem Özkan <ethemcem.ozkan@gmail.com>
2024-10-19 18:07:02 +0200

    Update models.py

--- src/tadoasync/models.py
+++ src/tadoasync/models.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass, field
-from typing import Any, Optional
+from typing import Any
 
 from mashumaro import field_options
 from mashumaro.mixins.orjson import DataClassORJSONMixin
